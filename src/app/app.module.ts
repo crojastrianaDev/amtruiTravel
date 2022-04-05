f0fe1ca66d1a7eadc66db21e8e431cdecc038c93
crojastriana3 <cristianrojas@unimayor.edu.co>
2022-04-05 17:33:29 -0500

    shareds ok

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FooterComponent } from './shared/footer/footer.component';
+import { HeaderComponent } from './shared/header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InicioComponent,
+    FooterComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
